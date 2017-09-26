0cf3f0094d6fda65a203e5a0e10af2ca65896a9d
HIlenet <hnet@NOmac.local>
2017-09-26 21:03:21 +0900

    [init] make setup script

---
+++ setup.sh
@@ -0,0 +1,25 @@
+# link dotfiles/.* files to $HOME/.*
+
+# except refs name
+except=("." ".." ".git" "gitignore")
+
+path=$(cd $(dirname $0);pwd)
+
+for f in $HOME/dotfiles/.* ;  do
+  flag=0
+  filename=${f#$path\/}
+
+  for e in "${except[@]}" ; do
+    if [ $filename = $e ] ; then
+      flag=1
+      break
+    fi
+  done
+  
+  if [ $flag = 1 ] ; then
+    continue
+  fi
+
+  echo "ln -s $f $HOME/$filename"
+  ln -s $f $HOME/$filename
+done
